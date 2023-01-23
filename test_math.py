6db06c96836696145934c850baff893a2c503918
reposatgithub <anoopsharma921@gmail.com>
2023-01-23 16:08:28 +1300

    Added First Math Test

---
+++ test_math.py
@@ -0,0 +1,11 @@
+"""
+This module contains basic unit tests for math operations. 
+Their purpose is to show hot to use the ptest framework by example.
+"""
+
+#----------------------------------------------------------------------------
+# A most basic test function
+#----------------------------------------------------------------------------
+
+def test_one_plus_one():
+    assert 1 + 1  == 2
